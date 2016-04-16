f762d0dd623829823c1159b98eeff2581717be60
rubenlucas93 <ruben.lucas.zaragoza@hotmail.com>
2016-04-16 03:59:04 +0200

    calculadora financiera

---
+++ src/src/appinventor/ai_ruben_lucas_zaragoza/calculadora_app_version2/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"150","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"19","AlignHorizontal":"3","AlignVertical":"2","AppName":"calculadora","BackgroundColor":"&HFF444444","Scrollable":"True","Sizing":"Responsive","Title":"inicio","Uuid":"0","$Components":[{"$Name":"Button4","$Type":"Button","$Version":"6","Text":"Conversor de tipos","Uuid":"863209062"},{"$Name":"Button2","$Type":"Button","$Version":"6","Height":"-1025","Width":"-1038","Text":"Calculadora","Uuid":"-1138671114"}]}}
+|#
